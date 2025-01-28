4be42a42d0c85510ef96948774568b0c340904f5
Lekc Rg <lekcrg@ya.ru>
2025-01-28 06:16:26 +0300

    iter2 create agent: system monitoring and report system monitorin to server, rand num, send counter system report

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"sync"
+
+	"github.com/LekcRg/metrics/internal/agent/metrics"
+	"github.com/LekcRg/metrics/internal/agent/runtimeMonitoring"
+)
+
+var pollInterval = 2
+var reportInterval = 10
+
+func main() {
+	var monitor map[string]float64
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go runtimeMonitoring.Start(&monitor, pollInterval)
+	// wg.Done() // to stop
+	go metrics.StartSending(&monitor, reportInterval)
+	// wg.Done() // to stop
+	wg.Wait()
+}
---
+++ internal/agent/metrics/metrics.go
@@ -0,0 +1,48 @@
+package metrics
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand/v2"
+	"net/http"
+	"time"
+)
+
+var baseUrl = "http://localhost:8080/update"
+var pollCountUrl = "http://localhost:8080/update/counter/PollCount/1"
+var countSent = 0
+
+func postRequest(url string) {
+	_, err := http.Post(url, "text/plain", bytes.NewBuffer([]byte("")))
+	if err != nil {
+		fmt.Printf("Url: %s\nError making http request: %s\n\n", url, err)
+	}
+}
+
+func sendMetric(mType string, name string, value string) {
+	countSent++
+	url := fmt.Sprintf(`%s/%s/%s/%s`, baseUrl, mType, name, value)
+	postRequest(url)
+}
+
+func getRandomUrl() string {
+	randomValueLeft := rand.IntN(99999)
+	randomValueRight := rand.IntN(999)
+	return fmt.Sprintf("%d.%d", randomValueLeft, randomValueRight)
+}
+
+func StartSending(monitor *map[string]float64, interval int) {
+	time.Sleep(time.Duration(interval) * time.Second)
+
+	for {
+		for key, value := range *monitor {
+			url := fmt.Sprintf(`http://localhost:8080/update/gauge/%s/%f`, key, value)
+			postRequest(url)
+			postRequest(pollCountUrl)
+		}
+
+		sendMetric("gauge", "RandomValue", getRandomUrl())
+		fmt.Printf("%d time sent\n", countSent)
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
+}
