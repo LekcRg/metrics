e8926bfd2b7e824d9b5bb6c6b81b481985868270
Lekc Rg <lekcrg@ya.ru>
2025-02-24 20:28:41 +0300

    iter7 agent: add waiting get monitoring data for first send; send metrics with json

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -12,8 +12,12 @@ func main() {
 	var monitor map[string]float64
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go monitoring.Start(&monitor, pollInterval)
+
+	readySignal := make(chan bool)
+	go monitoring.Start(&monitor, pollInterval, readySignal)
 	// wg.Done() // to stop
+	// wait get monitoring
+	<-readySignal
 	go metrics.StartSending(&monitor, reportInterval, addrFlag, https)
 	// wg.Done() // to stop
 	wg.Wait()
--- internal/agent/metrics/metrics.go
+++ internal/agent/metrics/metrics.go
@@ -1,31 +1,52 @@
 package metrics
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"math/rand/v2"
 	"net/http"
 	"time"
+
+	"github.com/LekcRg/metrics/internal/logger"
+	"github.com/LekcRg/metrics/internal/models"
+	"github.com/LekcRg/metrics/internal/server/storage"
 )
 
-func postRequest(url string) {
-	resp, err := http.Post(url, "text/plain", nil)
+func postRequest(url string, body io.Reader) {
+	resp, err := http.Post(url, "application/json", body)
 	if err != nil {
-		fmt.Printf("Url: %s\nError making http request: %s\n\n", url, err)
+		logger.Log.Error("Error making http request")
 		return
 	}
 	resp.Body.Close()
 }
 
-func sendMetric(mType string, name string, value string, baseURL string, countSent *int) {
-	*countSent++
-	url := fmt.Sprintf(`%s/%s/%s/%s`, baseURL, mType, name, value)
-	postRequest(url)
+func sendMetric(
+	mType string, name string,
+	value *storage.Gauge, delta *storage.Counter,
+	baseURL string,
+) {
+	body := models.Metrics{
+		ID:    name,
+		MType: mType,
+		Value: value,
+		Delta: delta,
+	}
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		fmt.Println()
+	}
+
+	postRequest(baseURL, bytes.NewBuffer(jsonBody))
 }
 
-func getRandomURL() string {
-	randomValueLeft := rand.IntN(99999)
-	randomValueRight := rand.IntN(999)
-	return fmt.Sprintf("%d.%d", randomValueLeft, randomValueRight)
+func getRandomValue() storage.Gauge {
+	randomValueLeft := storage.Gauge(rand.IntN(99999))
+	randomValueRight := storage.Gauge(float64(rand.IntN(999)) / 1000)
+
+	return randomValueLeft + randomValueRight
 }
 
 func StartSending(monitor *map[string]float64, interval int, addr string, https bool) {
@@ -37,15 +58,17 @@ func StartSending(monitor *map[string]float64, interval int, addr string, https 
 	}
 
 	countSent := 0
-	pollCountURL := baseURL + "/counter/PollCount/1"
 	for {
+		countSent++
 		for key, value := range *monitor {
-			url := fmt.Sprintf(`%s/gauge/%s/%f`, baseURL, key, value)
-			postRequest(url)
-			postRequest(pollCountURL)
+			sendVal := storage.Gauge(value)
+			sendMetric("gauge", key, &sendVal, nil, baseURL)
+			pollCountVal := storage.Counter(1)
+			sendMetric("counter", "PollCount", nil, &pollCountVal, baseURL)
 		}
 
-		sendMetric("gauge", "RandomValue", getRandomURL(), baseURL, &countSent)
+		randomVal := getRandomValue()
+		sendMetric("gauge", "RandomValue", &randomVal, nil, baseURL)
 		fmt.Printf("%d time sent\n", countSent)
 		time.Sleep(time.Duration(interval) * time.Second)
 	}
